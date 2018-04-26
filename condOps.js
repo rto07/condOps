69e9f3d8b0080f4c24f0b83769dd8054d5ea0ca2
Raul Torres <rto07@users.noreply.github.com>
2018-04-26 12:17:06 -0500

    Add files via upload
    Checkpoint 23 / Conditionals and Operators

---
+++ condOps.js
@@ -0,0 +1,10 @@
+let grade = 99;
+if (grade > 90 && grade < 100) {
+console.log  ("You received an A!");
+} else if 	(grade > 70) {
+console.log ("You received a B!");
+} else {
+console.log ("You received a F.");
+}
+
+//prints "You received an A!"
