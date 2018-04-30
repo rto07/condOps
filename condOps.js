ea25a3e422c9103055aa36811f85e730becad15c
Raul Torres <rto07@users.noreply.github.com>
2018-04-30 14:21:18 -0500

    Update condOps.js

--- condOps.js
+++ condOps.js
@@ -1,10 +1,16 @@
-let grade = 99;
-if (grade > 90 && grade < 100) {
-console.log  ("You received an A!");
-} else if 	(grade > 70) {
-console.log ("You received a B!");
-} else {
-console.log ("You received a F.");
+let grade = "A";
+  if (grade == "A") {
+    console.log("You received an A!");
+} else if (grade == "B"){
+  console.log("You received a B!");
+} else if (grade == "C") {
+  console.log("You received a C.");
+} else if (grade == "D") {
+  console.log ("You received a D.");
+} else if (grade =="F") {
+  console.log ("You received a F.");
 }
 
-//prints "You received an A!"
+/*Note to self: Use == for "grade" variable following intial establishment of variable value. 
+The "=" is only for setting the initial value of a variable. Also, make sure to set letter 
+values as string objects*/
