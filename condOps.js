e764cbf1c238fe64800a480297395827a917a6c3
Raul Torres <rto07@users.noreply.github.com>
2018-05-01 12:59:12 -0500

    Update condOps.js

--- condOps.js
+++ condOps.js
@@ -9,8 +9,15 @@ let grade = "A";
   console.log ("You received a D.");
 } else if (grade =="F") {
   console.log ("You received a F.");
+} else {console.log("You received an incompete.");
 }
 
-/*Note to self: Use == for "grade" variable following intial establishment of variable value. 
+
+
+/*Notes to self: 
+
+Use == for "grade" variable following intial establishment of variable value. 
 The "=" is only for setting the initial value of a variable. Also, make sure to set letter 
-values as string objects*/
+values as string objects
+
+Last 2 lines are "catch all" for any other operand entered into the variable value.*/
